fb2b6f6f2ae44b60dd30df04842e529970b4cb67
joshuahellier <joshuadmhellier@gmail.com>
2016-09-26 20:07:42 +0100

    Initially I tried to run straight away, but that didn't work, so instead I'm having an intermediate script do it.

--- codes/kmc/steadyStateFlow/kmcSubmit.sh
+++ codes/kmc/steadyStateFlow/kmcSubmit.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-qsub -cwd -l h_rt=0:20:0 yes -V -o "/dev/null" -e $RESULTS"/"$12"/errors/runErrors.out" python
+qsub -cwd -l h_rt=0:20:0 -V -o "/dev/null" -e $RESULTS"/"$12"/errors/runErrors.out" initKmc.sh
