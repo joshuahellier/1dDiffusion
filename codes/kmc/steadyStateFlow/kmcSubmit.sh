882b8ce283d810d6a303e880507502caf082590a
joshuahellier <joshuadmhellier@gmail.com>
2016-09-28 15:35:33 +0100

    Updated some stuff.

--- codes/kmc/steadyStateFlow/kmcSubmit.sh
+++ codes/kmc/steadyStateFlow/kmcSubmit.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+#
+# Made with a little help from Pat
+#
+#$ -cwd -V
+#$ -l h_rt=00:20:00
+#$ -R y
+
+
 #!/bin/sh
-qsub -cwd -l h_rt=0:20:0 -V -o "/dev/null" -e $RESULTS"/"$12"/errors/runErrors.out" initKmc.sh
+mkdir -p $RESULTS/steadyStateFlow/blinkTest3/0
+qsub -cwd -l h_rt=0:20:0 -V -e $RESULTS"/"$12"/errors/runErrors.out" /usr/bin/python steadyStateFlow.py 1.0 0.0 0.5 60 1 1000000 1.0 0.0 $RESULTS/steadyStateFlow/blinkTest3/0
