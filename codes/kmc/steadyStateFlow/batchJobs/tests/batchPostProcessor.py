12ed6b2d3a471bf4a9a67fc6bd60279141f420e6
joshuahellier <joshuadmhellier@gmail.com>
2016-10-04 17:43:34 +0100

    Done a little more, going home.

---
+++ codes/kmc/steadyStateFlow/batchJobs/tests/batchPostProcessor.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import numpy
+import subprocess
+
+
+resultDir = os.environ.get('RESULTS')
+if resultDir == None :
+    print "WARNING! $RESULTS not set! Attempt to write results will fail!\n"
+
+fileInfo = sys.argv[1]
+resultsPlace = resultDir+"/"+fileInfo+"/"
+
+if not os.path.exists(resultsPlace):
+    print "WARNING! The results directory requested does not exist! Perhaps there is some typo...\n"
+    exit()
+
+
+
+#Firstly, get rid of any files which could cause trouble and crash things
+
+
+topDirectoryList = os.listdir(resultsPlace)
+fullRate = 0.0
+avConc = 0.0
+
+jobIndex = 1
+
+for topLevel in topDirectoryList:
+    fileName = resultsPlace+topLevel+"/overallData.dat"
+    try:
+        os.remove(fileName)
+    except OSError:
+        pass
+    midDirectoryList = os.listdir(resultsPlace+topLevel)
+    for midLevel in midDirectoryList:
+        location = fileInfo+"/"+topLevel+"/"+midLevel
+        jobInput = "postProcessor.py "+location+"\n"
+        with open("postProcInputs/postProcInput."+str(jobIndex), 'w') as f:
+            f.write(jobInput)
+        jobIndex += 1
--- codes/tests/testJob.array.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ~/research/PhDStuff/codes/tests
-./testRunScript.sh $1.$SGE_TASK_ID
