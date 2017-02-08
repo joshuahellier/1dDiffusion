86e9f08317b3650ed7de0b92fb8315b51e7b1060
Joshua Hellier <joshuadmhellier@gmail.com>
2017-02-08 12:18:16 +0000

    Gathering stuff together, now that Eddie3's file staging has been completely changed.

--- codes/kmc/batchJobs/mainStuff/imagerInputCreator.py
+++ codes/kmc/batchJobs/mainStuff/imagerInputCreator.py
@@ -9,16 +9,14 @@ numLambda = 12
 numImageSteps=1000000
 dataLocation = "batchJobs/mainRuns/attempt6"
 newWriteLocation = "batchJobs/imagingRuns/attempt6"
-
+timeInterval = 1.0
 jobIndex = 1
 
 runningJobs = []
 
 for rateIndex in range(0, numLambda):
-    currentRate = lambdaMin + rateStepSize*rateIndex
     for concIndex in range(0, numConcs):
-        currentConc = concMin + concStepSize*concIndex
-        jobInput = "newPostProcIm.py "+str(numImageSteps)+" "+dataLocation+str(rateIndex)+"/"+str(concIndex)+"/"+str(0)+" "+newWriteLocation+str(rateIndex)+"/"+str(concIndex)+"/"+str(0)+"\n"
-            with open("jobInputs/testInput."+str(jobIndex), 'w') as f:
-                f.write(jobInput)
+        jobInput = "steadyFlowImager.py "+dataLocation+"/"+str(rateIndex)+"/"+str(concIndex)+"/"+str(0)+" "+str(numImageSteps)+" "+str(timeInterval)+" "+newWriteLocation+"/"+str(rateIndex)+"/"+str(concIndex)+"/"+str(0)+"\n"
+        with open("jobInputs/testInput."+str(jobIndex), 'w') as f:
+            f.write(jobInput)
         jobIndex += 1
--- codes/kmc/batchJobs/mainStuff/kmcSubmit.sh
+++ codes/kmc/batchJobs/mainStuff/kmcSubmit.sh
@@ -2,4 +2,4 @@
 #
 # Made with a little help from Pat
 #
-qsub -t 3-4608 -cwd -e "/dev/null" -o "/dev/null"  -l h_rt=08:00:00 kmcJobArray.sh "jobInputs/testInput"
+qsub -t 3-4608 -e "/dev/null" -o "/dev/null" -cwd -l h_rt=30:00:00 kmcJobArray.sh "jobInputs/testInput"
