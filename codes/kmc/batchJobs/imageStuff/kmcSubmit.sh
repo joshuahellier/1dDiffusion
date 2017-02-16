e387cb257429dbc866c3e9079b7b0253cea1a86b
joshuahellier <joshuadmhellier@gmail.com>
2017-02-16 15:05:34 +0000

    Whoops, just fixed kmcSubmit.sh

--- codes/kmc/batchJobs/imageStuff/kmcSubmit.sh
+++ codes/kmc/batchJobs/imageStuff/kmcSubmit.sh
@@ -2,4 +2,4 @@
 #
 # Made with a little help from Pat
 #
-qsub -t 3-4608 -e "/dev/null" -o "/dev/null" -cwd -l h_rt=30:00:00 kmcJobArray.sh "jobInputs/testInput"
+qsub -t 1-2 -e "/dev/null" -o "/dev/null" -cwd -l h_rt=00:10:00 kmcJobArray.sh "jobInputs/testInput"
