865677cc8091be35b6a158537842781964859cc6
Joshua Hellier <joshuadmhellier@gmail.com>
2017-03-23 16:51:07 +0000

    Completed the latest batch of Eddie3 calculations; tidying up.

--- codes/kmc/batchJobs/rateCaculation/mainStuff/kmcSubmit.sh
+++ codes/kmc/batchJobs/rateCaculation/mainStuff/kmcSubmit.sh
@@ -2,4 +2,4 @@
 #
 # Made with a little help from Pat
 #
-qsub -t 884-4608 -e "$RESULTS/batchJobs/mainRuns/errors/attempt6b" -o "/dev/null"  -cwd -l h_rt=25:00:00 kmcJobArray.sh "jobInputs/testInput"
+qsub -t 2975-4608 -e "$RESULTS/batchJobs/mainRuns/errors/attempt6b" -o "/dev/null"  -cwd -l h_rt=25:00:00 kmcJobArray.sh "jobInputs/testInput"
