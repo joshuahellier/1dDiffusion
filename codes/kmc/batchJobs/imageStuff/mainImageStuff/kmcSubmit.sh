d08f83a01c043c9091e61a10e2a1a369321b5394
Joshua Hellier <joshuadmhellier@gmail.com>
2017-05-25 14:23:01 +0100

    Housekeeping.

--- codes/kmc/batchJobs/imageStuff/mainImageStuff/kmcSubmit.sh
+++ codes/kmc/batchJobs/imageStuff/mainImageStuff/kmcSubmit.sh
@@ -2,4 +2,4 @@
 #
 # Made with a little help from Pat
 #
-qsub -t 1-288 -o "/dev/null" -e "/exports/eddie/scratch/s1373240/results/batchJobs/imagingRuns/errors"  -cwd -l h_rt=04:00:00 kmcJobArray.sh "jobInputs/testInput"
+qsub -t 1-1440 -o "/dev/null" -e "/exports/eddie/scratch/s1373240/results/batchJobs/imagingRuns/errors"  -cwd -l h_rt=04:00:00 kmcJobArray.sh "jobInputs/testInput"
