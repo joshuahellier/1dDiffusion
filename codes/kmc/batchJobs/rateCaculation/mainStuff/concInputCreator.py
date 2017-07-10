6939e58b9ca052e204fe50e2312f8d59fdc141b7
joshuahellier <joshuadmhellier@gmail.com>
2017-07-10 21:18:15 +0100

    Made some code in order to generate some new results for the paper.

---
+++ codes/kmc/batchJobs/rateCaculation/mainStuff/concInputCreator.py
@@ -0,0 +1,38 @@
+import subprocess
+import sys
+import os
+
+# This code is meant to manage running multiple instances of my KMCLib codes at the same time,
+# in the name of time efficiency
+numConcs = 16
+numLambda = 4
+numStepsEquilib = 160000000
+numStepsAnal = 80000000
+numStepsSnapshot = 1000
+numStepsReq = 16000000
+sysSize = 124
+analInterval = 1
+numPasses = 10
+timeInterval = 100.0
+dataLocation = "batchJobs/concRuns/attempt1/"
+lambdaMin = 0.1
+lambdaMax = 0.4
+rateStepSize = (lambdaMax-lambdaMin)/float(numLambda-1)
+concMax = 0.97
+concMin = 0.03
+concStepSize = (concMax-concMin)/float(numConcs-1)
+
+jobIndex = 1
+
+runningJobs = []
+
+for rateIndex in range(0, numLambda):
+    currentRate = lambdaMin + rateStepSize*rateIndex
+    for botConcIndex in range(0, numConcs):
+        botConc = concMin + concStepSize*botConcIndex
+        for topConcIndex in range(0, numConcs):
+            topConc = concMin + concStepSize*topConcIndex
+            jobInput = "concFlow.py "+str(botConc)+" "+str(topConc)+" "+str(currentRate)+" "+str(sysSize)+" "+str(analInterval)+" "+str(numStepsEquilib)+" "+str(numStepsSnapshot)+" "+str(numStepsAnal)+" "+str(numStepsReq)+" "+str(numPasses)+" "+str(timeInterval)+" "+dataLocation+str(rateIndex)+"/"+str(botConcIndex)+"/"+str(topConcIndex)+"\n"
+            with open("jobInputs/testInput."+str(jobIndex), 'w') as f:
+                f.write(jobInput)
+            jobIndex += 1
