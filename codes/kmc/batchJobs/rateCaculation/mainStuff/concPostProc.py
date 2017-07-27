fd67d9a98c34bbd4436a84a0e199eaf08395ec6e
joshuahellier <joshuadmhellier@gmail.com>
2017-07-27 18:47:11 +0100

    Ok, hopefully fixed now.

--- codes/kmc/batchJobs/rateCaculation/mainStuff/concPostProc.py
+++ codes/kmc/batchJobs/rateCaculation/mainStuff/concPostProc.py
@@ -4,6 +4,9 @@ import os
 
 # This code is meant to manage running multiple instances of my KMCLib codes at the same time,
 # in the name of time efficiency
+resultDir = os.environ.get('RESULTS')
+if resultDir == None :
+    print ("WARNING! $RESULTS not set! Attempt to write results will fail!\n")
 numConcs = 16
 numLambda = 4
 numStepsEquilib = 160000000
@@ -35,7 +38,7 @@ for rateIndex in range(0, numLambda):
         botConc = concMin + concStepSize*botConcIndex
         for topConcIndex in range(0, numConcs):
             topConc = concMin + concStepSize*topConcIndex
-            currentLoc = "$RESULTS/"+dataLocation+str(rateIndex)+"/"+str(botConcIndex)+"/"+str(topConcIndex)
+            currentLoc = resultDir+"/"+dataLocation+str(rateIndex)+"/"+str(botConcIndex)+"/"+str(topConcIndex)
             inTopVals = []
             outTopVals = []
             inBotVals = []
@@ -95,14 +98,14 @@ for rateIndex in range(0, numLambda):
                 stdErr = math.sqrt(squaredDev)/float(numPasses)
                 rateData.append([botConc, topConc, flowMean, stdErr])
             else:
-                failedRuns.append("$RESULTS/"+dataLocation+str(rateIndex)+"/"+str(botConcIndex)+"/"+str(topConcIndex)+"\n")
-    with open("$RESULTS/"+dataLocation+str(rateIndex)+"/rateMeans.proc", 'w') as f:
+                failedRuns.append(resultDir+"/"+dataLocation+str(rateIndex)+"/"+str(botConcIndex)+"/"+str(topConcIndex)+"\n")
+    with open(resultDir+"/"+dataLocation+str(rateIndex)+"/rateMeans.proc", 'w') as f:
         for index in rateData:
             f.write(str(rateData[index][0])+" "+str(rateData[index][1])+" "+str(rateData[index][2])+"\n")
-    with open("$RESULTS/"+dataLocation+str(rateIndex)+"/rateErrs.proc", 'w') as f:
+    with open(resultDir+"/"+dataLocation+str(rateIndex)+"/rateErrs.proc", 'w') as f:
         for index in rateData:
             f.write(str(rateData[index][0])+" "+str(rateData[index][1])+" "+str(rateData[index][3])+"\n")
 
-with open("$RESULTS/"+dataLocation+"failedRuns.proc", 'w') as f:
+with open(resultDir+"/"+dataLocation+"failedRuns.proc", 'w') as f:
     for index in failedRuns:
         f.write(index)
