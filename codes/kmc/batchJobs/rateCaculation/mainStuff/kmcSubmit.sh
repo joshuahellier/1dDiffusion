dccb84654df97f714104094016eb0c3d6dba3c3d
Joshua Hellier <joshuadmhellier@gmail.com>
2017-08-28 17:07:43 +0100

    About to chop off the unnecessary files from the git repository in order toi save space; will keep them locally on my laptop instead.

--- codes/kmc/batchJobs/rateCaculation/mainStuff/kmcSubmit.sh
+++ codes/kmc/batchJobs/rateCaculation/mainStuff/kmcSubmit.sh
@@ -2,4 +2,4 @@
 #
 # Made with a little help from Pat
 #
-qsub -t 3-3456 -e "/home/s1373240/research/results/errors" -o "/dev/null"  -cwd -l h_rt=30:00:00 kmcJobArray.sh "jobInputs/testInput"
+qsub -t 1-3456 -e "/home/s1373240/research/results/errors" -o "/dev/null"  -cwd -l h_rt=30:00:00 kmcJobArray.sh "jobInputs/testInput"
