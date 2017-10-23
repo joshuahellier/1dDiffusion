1cab09f14076dbd32c0aff3e5901e5c233ec7ff9
Joshua Hellier <joshuadmhellier@gmail.com>
2017-10-23 21:51:02 +0100

    Just saving the latest minor changes to Eddie's scripts.

--- codes/kmc/batchJobs/rateCaculation/mainStuff/kmcSubmit.sh
+++ codes/kmc/batchJobs/rateCaculation/mainStuff/kmcSubmit.sh
@@ -2,4 +2,4 @@
 #
 # Made with a little help from Pat
 #
-qsub -t 1-4096 -e "/home/s1373240/research/results/errors" -o "/dev/null"  -cwd -l h_rt=12:00:00 kmcJobArray.sh "jobInputs/testInput"
+qsub -t 1-300 -e "/home/s1373240/research/results/errors" -o "/dev/null"  -cwd -l h_rt=12:00:00 kmcJobArray.sh "jobInputs/testInput"
