8b8c2960e9189cb438c62e7c826e4bf67145b57a
joshuahellier <joshuadmhellier@gmail.com>
2017-10-25 14:35:25 +0100

    Cleaning up on Edinburgh's system.

--- codes/mathematica/constSliceDataAnal.m
+++ codes/mathematica/constSliceDataAnal.m
@@ -1,7 +1,7 @@
 (* ::Package:: *)
 
 (* ::Input:: *)
-(*loc="/home/s1373240/research/results/batchJobs/concRuns/constDensSlice1/";*)
+(*loc="/home/s1373240/research/results/batchJobs/concRuns/constDensSlice2/";*)
 (*means1 = Import[loc<>"densMeans.dat", "Data"];*)
 (*errs1 = Import[loc<>"densErrs.dat", "Data"];*)
 (*flow1 = Import[loc<>"rateMeans.dat", "Data"];*)
@@ -17,10 +17,17 @@
 
 
 (* ::Input:: *)
-(*ListDensityPlot[flowMeans1, InterpolationOrder->0, ColorFunction->"Rainbow", PlotLegends->True, PlotRange->{-0.005, 0.005}]*)
+(*ListDensityPlot[flowMeans1, InterpolationOrder->0, ColorFunction->"Rainbow", PlotLegends->True, PlotRange->Automatic]*)
 (*ListDensityPlot[flowVars1, InterpolationOrder->0, ColorFunction->"Rainbow", PlotRange->Automatic, PlotLegends->True]*)
 (*ListDensityPlot[flowSkew1, InterpolationOrder->0, ColorFunction->"Rainbow", PlotRange->Automatic, PlotLegends->True]*)
 (*ListDensityPlot[flowKurt1, InterpolationOrder->0, ColorFunction->"Rainbow", PlotRange->Automatic, PlotLegends->True]*)
+
+
+ListDensityPlot[means1, InterpolationOrder->0, ColorFunction->"Rainbow", PlotLegends->True, PlotRange->Automatic]
+ListDensityPlot[errs1, InterpolationOrder->0, ColorFunction->"Rainbow", PlotLegends->True, PlotRange->Automatic]
+ListDensityPlot[histMeans1, InterpolationOrder->0, ColorFunction->"Rainbow", PlotLegends->True, PlotRange->Automatic]
+ListDensityPlot[histErrs1, InterpolationOrder->0, ColorFunction->"Rainbow", PlotLegends->True, PlotRange->{0, 10000}]
+ListDensityPlot[blockDev1, InterpolationOrder->0, ColorFunction->"Rainbow", PlotLegends->True, PlotRange->Automatic]
 
 
 Manipulate[Show[{ListDensityPlot[flowMeans1, InterpolationOrder->0, ColorFunction->"Rainbow", PlotLegends->True, PlotRange->{-j, j}], ContourPlot[Sign[diff]J[2/3+diff, 2/3-diff, 1-l]/64==j, {l, 0, 0.6}, {diff, -0.33, 0.33}]}], {j, 10^-7, 0.01}]
