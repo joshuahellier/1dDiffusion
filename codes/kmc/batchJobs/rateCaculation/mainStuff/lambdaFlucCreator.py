c0f86242f2289ed8fd43077b9bef3e85503ae919
joshuahellier <joshuadmhellier@gmail.com>
2018-02-21 14:22:19 +0000

    Altered the lambdaScan code to do logarithmic lambda values; should help with scanning large lambdas.

--- codes/kmc/batchJobs/rateCaculation/mainStuff/lambdaFlucCreator.py
+++ codes/kmc/batchJobs/rateCaculation/mainStuff/lambdaFlucCreator.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import os
+import math
 
 # This code is meant to manage running multiple instances of my KMCLib codes at the same time,
 # in the name of time efficiency
@@ -13,18 +14,20 @@ sysSize = 64
 analInterval = 1
 numPasses = 10000
 timeInterval = 1.0
-dataLocation = "batchJobs/concRuns/lambdaScan5/"
-lambdaMin = 0.01
-lambdaMax = 0.7
+dataLocation = "batchJobs/concRuns/highLambdaScan1/"
+lambdaMin = 0.7
+lambdaMax = 30.0
+aParam = 1.0
 rateStepSize = (lambdaMax-lambdaMin)/float(numLambda-1)
-jobIndex = 4609
+jobIndex = 1
 
 runningJobs = []
 
 for rateIndex in range(0, numLambda):
-    currentRate = lambdaMin + rateStepSize*rateIndex
-    botConc = 0.99
-    topConc = 0.01
+    tempRate = lambdaMin + rateStepSize*rateIndex
+    currentRate = (lambdaMax - lambdaMin)*(math.exp(aParam*(tempRate-lambdaMin)/(lambdaMax-lambdaMin))-1.0)/(math.exp(aParam)-1) + lambdaMin
+    botConc = 0.75
+    topConc = 0.25
     jobInput = "concFlow.py "+str(botConc)+" "+str(topConc)+" "+str(currentRate)+" "+str(sysSize)+" "+str(analInterval)+" "+str(numStepsEquilib)+" "+str(numStepsSnapshot)+" "+str(numStepsAnal)+" "+str(numStepsReq)+" "+str(numPasses)+" "+str(timeInterval)+" "+dataLocation+str(rateIndex)+"\n"
     with open("jobInputs/testInput."+str(jobIndex), 'w') as f:
         f.write(jobInput)
