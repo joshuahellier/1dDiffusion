d2206255ce45d3edac7719b1d21f06e656dac7ee
Joshua Hellier <joshuadmhellier@gmail.com>
2018-04-05 14:58:09 +0100

    Tidying up on Eddie before adding new analysis code for 2d energy calculations.

--- codes/kmc/batchJobs/rateCaculation/mainStuff/lambdaFlucCreator.py
+++ codes/kmc/batchJobs/rateCaculation/mainStuff/lambdaFlucCreator.py
@@ -14,20 +14,20 @@ sysSize = 64
 analInterval = 1
 numPasses = 10000
 timeInterval = 1.0
-dataLocation = "batchJobs/concRuns/extremeLambdaScan1/"
+dataLocation = "batchJobs/concRuns/extremeLambdaScan3/"
 lambdaMin = 1000.0
 lambdaMax = 1000000000.0
 aParam = 1.0
 rateStepSize = (lambdaMax-lambdaMin)/float(numLambda-1)
-jobIndex = 1
+jobIndex = 257
 
 runningJobs = []
 
 for rateIndex in range(0, numLambda):
     tempRate = lambdaMin + rateStepSize*rateIndex
     currentRate = math.exp(((tempRate-lambdaMin)*math.log(lambdaMax)+(lambdaMax-tempRate)*math.log(lambdaMin))/(lambdaMax-lambdaMin))
-    botConc = 0.5
-    topConc = 0.3
+    botConc = 0.9
+    topConc = 0.7
     jobInput = "concFlow.py "+str(botConc)+" "+str(topConc)+" "+str(currentRate)+" "+str(sysSize)+" "+str(analInterval)+" "+str(numStepsEquilib)+" "+str(numStepsSnapshot)+" "+str(numStepsAnal)+" "+str(numStepsReq)+" "+str(numPasses)+" "+str(timeInterval)+" "+dataLocation+str(rateIndex)+"\n"
     with open("jobInputs/testInput."+str(jobIndex), 'w') as f:
         f.write(jobInput)
